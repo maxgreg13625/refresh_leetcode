3122edecf31c313569479a48ba4f2d934e2c3a1b
Rendy <maxgreg13625@gmail.com>
2024-04-05 22:57:38 +0800

    first init word_search

--- 079/python/main.py
+++ 079/python/main.py
@@ -1,8 +1,33 @@
 from typing import List
 
 class Solution:
+    def check_element(self, i: int, j: int, w_idx: int) -> bool:
+        if i < 0 or j < 0 or i >= self.height or j >= self.width:
+            return False
+
+        if self.board[i][j] != self.word[w_idx]:
+            return False
+        elif self.board[i][j] == self.word[w_idx] and w_idx == self.length:
+            return True
+        else:
+            w_idx += 1
+            
+            # check top, down, left, and right 
+            return self.check_element(i-1, j, w_idx) or\
+                self.check_element(i+1, j, w_idx) or\
+                self.check_element(i, j-1, w_idx) or\
+                self.check_element(i, j+1, w_idx)
+
     def exist(self, board: List[List[str]], word: str) -> bool:
-        pass
+        self.board, self.word = board, word
+        self.height, self.width, self.length = len(board), len(board[0]), len(word)
+
+        word_idx = 0
+        for i in range(self.height):
+            for j in range(self.width):
+                if self.check_element(i, j, word_idx):
+                    return True
+        return False
 
 def main():
     board = [["A", "B", "C", "E"],
