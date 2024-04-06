68fbd9fc23a0deb705c6ad41880ca6101e5958dd
Rendy <maxgreg13625@gmail.com>
2024-04-06 16:30:36 +0800

    first pass

--- 079/python/main.py
+++ 079/python/main.py
@@ -7,36 +7,65 @@ class Solution:
 
         if self.board[i][j] != self.word[w_idx]:
             return False
-        elif self.board[i][j] == self.word[w_idx] and w_idx == self.length:
-            return True
         else:
             w_idx += 1
-            
-            # check top, down, left, and right 
-            return self.check_element(i-1, j, w_idx) or\
-                self.check_element(i+1, j, w_idx) or\
-                self.check_element(i, j-1, w_idx) or\
-                self.check_element(i, j+1, w_idx)
+            if w_idx == len(self.word):
+                return True
+            else:
+                # replace original char to avoid back to current path
+                temp, self.board[i][j] = self.board[i][j], ''
+                # check top, down, left, and right
+                if self.check_element(i-1, j, w_idx) or\
+                        self.check_element(i+1, j, w_idx) or\
+                        self.check_element(i, j-1, w_idx) or\
+                        self.check_element(i, j+1, w_idx):
+                    return True
+                else:
+                    self.board[i][j] = temp
+                    return False
 
     def exist(self, board: List[List[str]], word: str) -> bool:
         self.board, self.word = board, word
         self.height, self.width, self.length = len(board), len(board[0]), len(word)
 
-        word_idx = 0
+        # if the word length large than board size, return False directly
+        if self.height * self.width < self.length:
+            return False
+ 
         for i in range(self.height):
             for j in range(self.width):
-                if self.check_element(i, j, word_idx):
+                if self.check_element(i, j, 0):
                     return True
         return False
 
 def main():
+    test = Solution()
+    # case 1
     board = [["A", "B", "C", "E"],
              ["S", "F", "C", "S"],
              ["A", "D", "E", "E"]]
-    test = Solution()
     assert test.exist(board, "ABCCED") == True
+    # case 2
+    board = [["A", "B", "C", "E"],
+             ["S", "F", "C", "S"],
+             ["A", "D", "E", "E"]]
     assert test.exist(board, "SEE") == True
+    # case 3
+    board = [["A", "B", "C", "E"],
+             ["S", "F", "C", "S"],
+             ["A", "D", "E", "E"]]
     assert test.exist(board, "ABCB") == False
-
+    # case 4
+    board = [["a", "a", "a", "a"],
+             ["a", "a", "a", "a"],
+             ["a", "a", "a", "a"]]
+    assert test.exist(board, "aaaaaaaaaaaaa") == False
+    # case 5
+    board = [["A", "A"],
+             ["A", "a"],
+             ["A", "a"],
+             ["a", "a"],
+             ["a", "A"]]
+    assert test.exist(board, "AaaaaAaAA") == False
 if __name__ == '__main__':
     main()
