5e3c87aea98c67333d633584d46f6b56d5f81506
Rendy <maxgreg13625@gmail.com>
2024-04-10 23:02:48 +0800

    add 035

---
+++ 035/python/main.py
@@ -0,0 +1,28 @@
+from typing import List
+
+class Solution:
+    def binary_search(self, nums: List[int], target: int, start: int, end: int):
+        medium = (start + end) // 2
+
+        if nums[medium] == target:
+            return medium
+        elif nums[medium] < target and medium == start:
+            return medium + 1
+        elif nums[medium] < target:
+            return self.binary_search(nums, target, medium, end)
+        else:
+            return self.binary_search(nums, target, start, medium)
+
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        length = len(nums)
+
+        return self.binary_search(nums, target, 0, length)
+
+def main():
+    test = Solution()
+    assert test.searchInsert([1, 3, 5, 6], 5) == 2
+    assert test.searchInsert([1, 3, 5, 6], 2) == 1
+    assert test.searchInsert([1, 3, 5, 6], 7) == 4
+
+if __name__ == '__main__':
+    main()
