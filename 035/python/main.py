b7efb3b9ee32da025c97a7bb0a6992d0d5d0e3d6
Rendy <maxgreg13625@gmail.com>
2024-04-11 06:45:26 +0800

    recursive pass

--- 035/python/main.py
+++ 035/python/main.py
@@ -8,14 +8,16 @@ class Solution:
             return medium
         elif nums[medium] < target and medium == start:
             return medium + 1
+        elif nums[medium] > target and medium == start:
+            return medium
         elif nums[medium] < target:
             return self.binary_search(nums, target, medium, end)
         else:
             return self.binary_search(nums, target, start, medium)
 
     def searchInsert(self, nums: List[int], target: int) -> int:
+        # solution 1: recursive
         length = len(nums)
-
         return self.binary_search(nums, target, 0, length)
 
 def main():
@@ -23,6 +25,7 @@ def main():
     assert test.searchInsert([1, 3, 5, 6], 5) == 2
     assert test.searchInsert([1, 3, 5, 6], 2) == 1
     assert test.searchInsert([1, 3, 5, 6], 7) == 4
+    assert test.searchInsert([1, 3, 5, 6], 0) == 0
 
 if __name__ == '__main__':
     main()
